dfdd1f1fe31b5b3fafa4dddfb968065a32aa9773
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-03-23 14:57:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Require the Bot module
 const Bot = require('./src/Bot');
+
+// Create a new instance of the Bot class
 const bot = new Bot();
 
+// Launch the bot
 bot.telegraf.launch();
